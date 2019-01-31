79cdf946dddbeeaa788b395e28634708093fdfa1
naman1399 <naman.gupta1399@gmail.com>
2019-01-31 23:58:59 +0530

    new commit

---
+++ Classes/src/com/namangupta/Car.java
@@ -0,0 +1,20 @@
+package com.namangupta;
+
+public class Car {
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String colour;
+
+    public void setModel(String model){
+        this.model=model;
+    }
+
+    public String getModel(){
+        return this.model;
+    }
+
+
+}
--- Classes/src/com/namangupta/Main.java
+++ Classes/src/com/namangupta/Main.java
@@ -3,6 +3,11 @@ package com.namangupta;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Car porsche = new Car();
+        Car holden = new Car();
+        porsche.setModel("Carrera");
+
+
     }
 }
