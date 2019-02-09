67644d7b93b6b3b33d4df18439f362320e224034
naman1399 <naman.gupta1399@gmail.com>
2019-02-10 00:37:35 +0530

    new commit

---
+++ Inheritance/src/com/namangupta/Main.java
@@ -0,0 +1,13 @@
+package com.namangupta;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Animal animal = new Animal("Animal", 1, 1, 5, 5);
+
+        Dog dog = new Dog("Yorkie", 8, 8, 2, 4, 1, 20, "long silky");
+        dog.eat();
+
+    }
+}
