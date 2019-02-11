3f323b9fa1612802cb5beb42d0df9865c3874724
naman1399 <naman.gupta1399@gmail.com>
2019-02-11 23:13:31 +0530

    new commit

---
+++ Composition/src/com/namangupta/Lamp.java
@@ -0,0 +1,30 @@
+package com.namangupta;
+
+public class Lamp {
+
+    private String style;
+    private boolean battery;
+    private int globrating;
+
+    public Lamp(String style, boolean battery, int globrating) {
+        this.style = style;
+        this.battery = battery;
+        this.globrating = globrating;
+    }
+
+    public void turnOn() {
+        System.out.println("Lamp -> Turning on");
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public boolean isBattery() {
+        return battery;
+    }
+
+    public int getGlobrating() {
+        return globrating;
+    }
+}
