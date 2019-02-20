45d56e3ca082183bcf593069e1faa8c58af2e5ce
naman1399 <naman.gupta1399@gmail.com>
2019-02-21 00:31:16 +0530

    new commit

---
+++ Encapsulation/src/com/namangupta/Player.java
@@ -0,0 +1,21 @@
+package com.namangupta;
+
+public class Player {
+
+    public String fullName;
+    public int health;
+    public String weapon;
+
+    public void loseHealth(int damage) {
+        this.health -= damage;
+        if (this.health <= 0) {
+            System.out.println("Player knocked out");
+            // Reduce number of lives for the player
+        }
+    }
+
+    public int healthRemaining() {
+        return this.health;
+    }
+
+}
