da1ea8417e1b42d143d597ae6c87ccbf7a3f22c3
naman1399 <naman.gupta1399@gmail.com>
2019-02-21 00:59:26 +0530

    new commit

---
+++ EncapsulationChallenge/src/com/namangupta/Main.java
@@ -0,0 +1,15 @@
+package com.namangupta;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Printer printer = new Printer(50, false);
+        System.out.println("inital page count = " + printer.getPagesPrinted());
+        int pagesPrinted = printer.printPages(4);
+        System.out.println("Pages printed was " + pagesPrinted + " new total print count for printer = " + printer.getPagesPrinted());
+        pagesPrinted = printer.printPages(2);
+        System.out.println("Pages printed was " + pagesPrinted + " new total print count for printer = " + printer.getPagesPrinted());
+
+    }
+}
